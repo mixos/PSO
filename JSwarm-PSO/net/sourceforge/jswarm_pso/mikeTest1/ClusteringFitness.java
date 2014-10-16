ee61cfd0906ae6e352ee0d9b56bf98cce618829c
mike_x_ <mixos.x@gmail.com>
2014-10-16 16:14:43 +0300

    cluster fitness phase 1

--- JSwarm-PSO/net/sourceforge/jswarm_pso/mikeTest1/ClusteringFitness.java
+++ JSwarm-PSO/net/sourceforge/jswarm_pso/mikeTest1/ClusteringFitness.java
@@ -1,5 +1,7 @@
 package sourceforge.jswarm_pso.mikeTest1;
 
+import java.util.ArrayList;
+
 import sourceforge.jswarm_pso.FitnessFunction;
 
 public class ClusteringFitness extends FitnessFunction {
@@ -15,8 +17,47 @@ public class ClusteringFitness extends FitnessFunction {
 
 	@Override
 	public double evaluate(double[] position) {
-		// TODO Auto-generated method stub
+		int clNo = InitExecution.NUMBER_OF_CLUSTERS;
+		int dims = Dataset.dimensions;
+		double[] Ck = new double[clNo];
+		int[] counter = new int[clNo];
+		double centerDist = 0.0;
+		ArrayList<double[]> centers = new ArrayList<double[]>();
+				
+		for(int i=1;i<=clNo;i++){			
+			double[] center = new double[dims];
+			int start = (i-1)*dims;
+			int end = start+dims-1;
+			for(int j=start;j<=end;j++){
+				center[j-start] = position[j];
+			}
+			centers.add(center);
+		}
+		for(int i=1;i<=clNo;i++){
+			double[] ctemp1 = centers.get(i);
+			for(int j=i+1;j<=clNo;j++){
+				double[] ctemp2 = centers.get(j);
+				for(int k=0;k<ctemp1.length;k++){
+					centerDist += Math.pow(ctemp1[k]-ctemp2[k],2);
+				}
+			}
+			
+		}
+		
+		for(int i=0;i<Dataset.data.numInstances();i++){
+			
+		}
+		
 		return 0;
+	}
+	
+	private double euclideanDist(double[] cpos, double[] dpos){
+		double temp2 = 0.0;
+		for(int i=0;i<cpos.length;i++){
+			double temp1 = Math.pow(cpos[i]-dpos[i],2);
+			temp2 += temp1;
+		}
+		return Math.sqrt(temp2);
 	}
 
 }
