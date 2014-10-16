626cda0880b40ffc0a7d1cd915093c98b27acc5b
mike_x_ <mixos.x@gmail.com>
2014-10-16 17:20:21 +0300

    candidate 1

--- JSwarm-PSO/net/sourceforge/jswarm_pso/mikeTest1/ClusteringFitness.java
+++ JSwarm-PSO/net/sourceforge/jswarm_pso/mikeTest1/ClusteringFitness.java
@@ -1,6 +1,7 @@
 package sourceforge.jswarm_pso.mikeTest1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import sourceforge.jswarm_pso.FitnessFunction;
 
@@ -21,8 +22,10 @@ public class ClusteringFitness extends FitnessFunction {
 		int clNo = InitExecution.NUMBER_OF_CLUSTERS;
 		int dims = Dataset.dimensions;
 		int[] Ck = new int[clNo];
+		double distFromCenter = 0.0;
 		double centerDist = 0.0;
 		ArrayList<double[]> centers = new ArrayList<double[]>();
+		HashMap<Integer,Integer> pMap = new HashMap<Integer,Integer>();
 		
 		//isolate centers of a particle and add them to a list
 		for(int i=1;i<=clNo;i++){			
@@ -40,9 +43,10 @@ public class ClusteringFitness extends FitnessFunction {
 			double[] ctemp1 = centers.get(i);
 			for(int j=i+1;j<clNo;j++){
 				double[] ctemp2 = centers.get(j);
-				for(int k=0;k<ctemp1.length;k++){
+				/*for(int k=0;k<ctemp1.length;k++){
 					centerDist += Math.pow(ctemp1[k]-ctemp2[k],2);
-				}
+				}*/
+				centerDist += distance(ctemp1,ctemp2);
 			}
 			
 		}
@@ -69,10 +73,25 @@ public class ClusteringFitness extends FitnessFunction {
 				}
 			}
 			Ck[minCl]++;
+			pMap.put(i, minCl);
 		}
 		
 		//real fitness
+		for(int i=0;i<Dataset.data.numInstances();i++){
+			int theClusterNumber = pMap.get(i);
+			double[] theCluster = centers.get(theClusterNumber);
+			double[] thePos = new double[dims];
+			for(int d=0;d<dims;d++){					
+				if(Dataset.data.attribute(d).isNominal()){
+					thePos[d] = Utils.nMap.get(Dataset.data.attribute(d).name()).get(Dataset.data.instance(i).toString(d));
+				}else{
+					thePos[d] = Dataset.data.instance(i).value(d);
+				}
+			}
+			distFromCenter += distance(theCluster,thePos)/Ck[theClusterNumber];
+		}
 		
+		double finalValue = distFromCenter/centerDist;
 		return 0;
 	}
 	
@@ -83,6 +102,15 @@ public class ClusteringFitness extends FitnessFunction {
 			temp2 += temp1;
 		}
 		return Math.sqrt(temp2);
+	}
+	
+	private double distance(double[] cpos, double[] dpos){
+		double temp2 = 0.0;
+		for(int i=0;i<cpos.length;i++){
+			double temp1 = Math.pow(cpos[i]-dpos[i],2);
+			temp2 += temp1;
+		}
+		return temp2;
 	}
 
 }
